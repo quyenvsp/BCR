b4e179401908455f6927a2b3fa5f69995b430b07
quyenvsp <quyenvsp@gmail.com>
2023-10-19 15:36:13 +0700

    Version 1.74

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -105,7 +105,7 @@ val gitVersionTriple = describeVersion(git)
 val gitVersionCode = getVersionCode(gitVersionTriple)
 val gitVersionName = getVersionName(git, gitVersionTriple)
 
-val projectUrl = "https://github.com/chenxiaolong/BCR"
+val projectUrl = "https://github.com/quyenvsp/BCR"
 val releaseMetadataBranch = "master"
 
 val extraDir = layout.buildDirectory.map { it.dir("extra") }
